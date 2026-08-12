c28895e25ad12dde739f2656f2186f2d35e420b2
Juanda <charmin990530@gmail.com>
2026-08-11 20:36:12 -0500

    tercer ejercicio

--- ejercicios/index.js
+++ ejercicios/index.js
@@ -7,3 +7,9 @@ console.log("Último color:", colores[colores.length - 1]);
 const numeros = ["1", "2", "4", "5"]
 
 console.log (`el array tiene, ${numeros.length} elementos`);
+
+for (let i = 1; i <= 20; i = i + 1) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
