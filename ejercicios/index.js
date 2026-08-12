cd2786eb98aa2dcba9aafbfcce871d5060eb18f3
Juanda <charmin990530@gmail.com>
2026-08-11 20:20:14 -0500

    correccion

--- ejercicios/index.js
+++ ejercicios/index.js
@@ -6,4 +6,4 @@ console.log("Último color:", colores[colores.length - 1]);
 
 const numeros = ["1", "2", "4", "5"]
 
-console.log("tiene estos elementos", numeros.length, "elementos");
+console.log (`el array tiene, ${numeros.length} elementos`);
