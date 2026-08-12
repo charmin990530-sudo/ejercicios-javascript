b3630ed5dcc8cfeb5f8cfebd9db23c858d01f291
Juanda <charmin990530@gmail.com>
2026-08-11 19:54:10 -0500

    segundo ejercicio

--- ejercicios/index.js
+++ ejercicios/index.js
@@ -2,3 +2,8 @@ const colores = ["rojo", "verde", "azul", "amarillo", "violeta"];
 
 console.log("Primer color:", colores[0]);
 console.log("Último color:", colores[colores.length - 1]);
+
+
+const numeros = ["1", "2", "4", "5"]
+
+console.log("tiene estos elementos", numeros.length, "elementos");
